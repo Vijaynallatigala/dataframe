66e8a15045ca6a27d23b823bb6347b30df1648bc
vijay <vijaynallateegala@gmail.com>
2024-10-30 14:16:59 +0530

    this is my first df1 changed commit

--- src/main/scala/df1.scala
+++ src/main/scala/df1.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
 object df1 {
   def main(args:Array[String]):Unit={
@@ -8,12 +9,26 @@ object df1 {
       .master("local[*]")
       .getOrCreate()
 
+    //ddl approach
+//    val schema = "id Int, name String, age Int"
+
+   // programmatical approach
+    val schema = StructType(List(
+      StructField("id", IntegerType),
+      StructField("name", StringType),
+      StructField("age",IntegerType)
+
+    ))
     val df = spark.read.format("csv")
       .option("header", true)
+//      .option("inferschema",true)
+      .schema(schema)
       .option("path", "E:/data/details.csv")
       .load()
-    df.show()
+
     df.printSchema()
+    df.show()
+
 
 
 
